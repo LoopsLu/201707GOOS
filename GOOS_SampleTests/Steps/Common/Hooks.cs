60262713d235efba809962099c361fe1d39620f0
LoopsLu <beethovenlu@hotmail.com>
2017-07-01 13:12:09 +0800

    * Regract test case   - Add test Hook and "web" tag in Feature content

---
+++ GOOS_SampleTests/Steps/Common/Hooks.cs
@@ -0,0 +1,20 @@
+﻿using FluentAutomation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace GOOS_SampleTests.Steps.Common
+{
+    [Binding]
+    public sealed class Hooks
+    {
+        [BeforeFeature()]
+        [Scope(Tag = "web")]
+        public static void SetBrowser()
+        {
+            SeleniumWebDriver.Bootstrap(SeleniumWebDriver.Browser.Chrome);
+        }
+    }
+}

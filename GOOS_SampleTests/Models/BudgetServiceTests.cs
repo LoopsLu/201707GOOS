a0c4cae67afcfe88ccd4223a00a20bec17e5daa3
LoopsLu <beethovenlu@hotmail.com>
2017-07-01 17:43:01 +0800

    * BudgetServiceTests   - CreateTest_when_exist_record_should_update_budget   > Improve test to assert update event

--- GOOS_SampleTests/Models/BudgetServiceTests.cs
+++ GOOS_SampleTests/Models/BudgetServiceTests.cs
@@ -40,13 +40,24 @@ namespace GOOS_SampleTests.Models
         public void CreateTest_when_exist_record_should_update_budget()
         {
             this._budgetService = new BudgetService(_budgetRepositoryStub);
+
             var budgetFromDb = new Budget { Amount = 999, YearMonth = "2017-02" };
+
             _budgetRepositoryStub.Read(Arg.Any<Func<Budget, bool>>())
                 .ReturnsForAnyArgs(budgetFromDb);
+
             var model = new BudgetAddViewModel { Amount = 2000, Month = "2017-02" };
+
+            var wasUpdated = false;
+            this._budgetService.Updated += (sender, args) => { wasUpdated = true; };
+
             this._budgetService.Create(model);
+
             _budgetRepositoryStub.Received()
                 .Save(Arg.Is<Budget>(x => x == budgetFromDb && x.Amount == 2000));
+
+
+            Assert.IsTrue(wasUpdated);
         }
     }
 }

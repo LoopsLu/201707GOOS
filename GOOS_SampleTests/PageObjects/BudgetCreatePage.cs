17c96e7075b97b24d55c98bd436ef1138feae1f6
LoopsLu <beethovenlu@hotmail.com>
2017-07-01 11:23:58 +0800

    Add a fail test case of BugetCreate

---
+++ GOOS_SampleTests/PageObjects/BudgetCreatePage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FluentAutomation;
+
+namespace GOOS_SampleTests.PageObjects
+{
+    internal class BudgetCreatePage : PageObject<BudgetCreatePage>
+    {
+        public BudgetCreatePage(FluentTest test) : base(test)
+        {
+        }
+
+        internal BudgetCreatePage Amount(int amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal BudgetCreatePage Month(string yearMonth)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal BudgetCreatePage AddBudget()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal BudgetCreatePage ShouldDisplay(object message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
